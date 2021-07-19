039f53bdcd9ac5dbde1f687a72716a400b6b44f4
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-19 17:01:31 -0300

    OrkutNostalgicIconSet - Quantidade de emojis ativos alterados 😅😎

--- src/lib/AlurakutCommons.js
+++ src/lib/AlurakutCommons.js
@@ -287,7 +287,8 @@ export function OrkutNostalgicIconSet(props) {
         { name: "Legal", slug: "legal", icon: "cool" },
         { name: "Sexy", slug: "sexy", icon: "heart" }
       ].map(({ name, slug, icon }) => {
-        const total = props[slug] ? props[slug] : 2;
+        /* ícones ativos: */
+        const total = props[slug] ? props[slug] : 3;
         return (
           <li key={`orkut__icon_set__${slug}`}>
             <span className="OrkutNostalgicIconSet__title">{name}</span>
