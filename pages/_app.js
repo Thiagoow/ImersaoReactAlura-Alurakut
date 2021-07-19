aac353c8882bef57e1f65481b30e9ab6375d5a97
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-07-19 15:11:08 -0300

    Nos 50:45 :D

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,31 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+/* Reset CSS */
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
   body {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+    font-family: sans-serif;
+    background-color: #D9E6F6;
   }
-`
+
+/* Definindo um pouco do layout padrão */
+#__next {
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+}
+`;
 
 const theme = {
   colors: {
-    primary: '#0070f3',
-  },
-}
+    primary: "#0070f3"
+  }
+};
 
 export default function App({ Component, pageProps }) {
   return (
@@ -22,5 +35,5 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const Box = styled.div`
+  background: #fff;
+  font-size: 2rem;
+  border-radius: 8px;
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <main align="center">
+      <Box>Title</Box>
+      <Box>Bem vindo</Box>
+      <Box>Bem vindo</Box>
+      <Box>Yey :D</Box>
+    </main>
+  );
 }
